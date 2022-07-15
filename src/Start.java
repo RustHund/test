a834e17a860ecb7b3eab28a5d3efc8008fad886f
RustHund <anatolykukhtenkov@gmail.com>
2022-07-15 17:39:30 +0300

    Commit 2

--- src/Start.java
+++ src/Start.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Start {
+    public static void math (int args){
+        System.out.println (args);
+    }
     public static void main (String[] args){
         System.out.println ("RustHund in IT");
+        math(43);
     }
 
 }
